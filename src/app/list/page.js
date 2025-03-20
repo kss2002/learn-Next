568c750c79c2e5851f9e28258366dd4e02109077
seong su kim <know.warehouse02@gmail.com>
2025-03-20 12:00:01 +0900

    first commit

---
+++ src/app/list/page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function List() {
+  let food = ['상품 1 40,000원', '상품 2 36,000원'];
+
+  return (
+    <>
+      <h1 className="hello">상품 목록</h1>
+      <div className="food">
+        <h4>{food[0]}</h4>
+      </div>
+      <div className="food">
+        <h4>{food[1]}</h4>
+      </div>
+      <Link className="Link" href="/">
+        Home
+      </Link>
+    </>
+  );
+}
