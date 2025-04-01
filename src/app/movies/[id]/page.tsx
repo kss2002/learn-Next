08ddceca9eed78810b74c239efa9291a4b4edcbe
seong su kim <know.warehouse02@gmail.com>
2025-04-02 02:12:19 +0900

    Suspense add

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,13 +1,32 @@
-'use client';
+import { Suspense } from 'react';
+import MovieInfo, { getMovie } from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
 
-import { useParams } from 'next/navigation';
-
-export default function MovieDetail() {
-  const params = useParams();
-  const id = params.id as string; // type
-
-  return <h1>Movie {id}</h1>;
+interface Iparams {
+  params: { id: string };
 }
+
+export async function generateMetadata({ params: { id } }: Iparams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: Iparams) {
+  return (
+    <div>
+      <Suspense fallback={<h1>info 로딩 중..</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>video 로딩 중..</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
+}
+
+export const runtime = 'edge';
 
 // [id] < 이게 다이나믹 라우팅하는 방법. so ezzzzzzzzzzzzz!
 // console를 찍으면 백엔드에서 id를 받아오는 것을 확인 가능.
