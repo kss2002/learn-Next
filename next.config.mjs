723fff28975b91dbc56e6c13332f9193a79379ef
seong su kim <know.warehouse02@gmail.com>
2025-04-03 14:07:30 +0900

    page.tsx / type

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // React Strict 모드 활성화
-
-  // 앱 디렉토리 활성화 (Next.js 13 이상에서 사용하는 경우)
-  // 앱 디렉토리를 사용하려면 app 디렉토리가 있어야 함
-  experimental: {
-    appDir: true,
-  },
-
-  // 최신 Next.js 버전에서 자동으로 SWC로 최적화되므로 별도로 설정할 필요 없음
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,36 +1,37 @@
-import Link from 'next/link';
+import Movie from '../../../components/movie';
+import styles from '../../styles/home.module.css';
 import { API_URL } from '../constants';
 
 export const metadata = {
   title: 'Home',
 };
-
-interface Movie__type {
+interface MovieType {
   id: string;
+  poster_path: string;
   title: string;
 }
 
-async function getMovies(): Promise<Movie__type[]> {
+async function getMovies(): Promise<MovieType[]> {
   const response = await fetch(API_URL);
-  const json = await response.json();
+  const json: MovieType[] = await response.json();
   return json;
 }
 
-export default async function Home() {
-  const movies = await getMovies();
+export default async function HomePage() {
+  const movies: MovieType[] = await getMovies(); // ✅ 타입 지정
+
   return (
-    <>
-      <div>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        ))}
-      </div>
-    </>
+    <div className={styles.container}>
+      {movies.map((movie: MovieType) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
+      ))}
+    </div>
   );
 }
 
-// server side data fetch
-// async 비동기 처리에는 await가 필요해진다.
-// typescript에서는 type를 명시적으로 선언한다.
+export const runtime = 'edge';
