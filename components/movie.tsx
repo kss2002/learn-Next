9a0613ade03a5921174d3dfbf3769b192e9a436e
seong su kim <know.warehouse02@gmail.com>
2025-04-11 21:47:09 +0900

    image 최적화

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/movie.module.css';
 import { useRouter } from 'next/navigation';
@@ -17,7 +18,14 @@ export default function Movie({ title, id, poster_path }: IMovieProps) {
   };
   return (
     <div className={styles.movie}>
-      <img src={poster_path} alt={title} onClick={onClick} />
+      <Image
+        width={300}
+        height={450}
+        src={poster_path}
+        alt={title}
+        onClick={onClick}
+        priority
+      />
       <Link prefetch href={`/movies/${id}`}>
         {title}
       </Link>
